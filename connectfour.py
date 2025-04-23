bc609f2639710566f6467d88cbfa7174a2466cee
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2025-04-23 11:00:51 +0200

    Update connectfour.py

--- connectfour.py
+++ connectfour.py
@@ -14,7 +14,7 @@ def main(session):
 
     try:
         # Pfad zur HTML-Datei
-        html_file_path = "http://198.18.0.1/apps/voice_handler/connectfour.html"
+        html_file_path = "http://198.18.0.1/apps/pepper-web-games/connectfour.html"
 
         # Anzeigen der HTML-Seite
         tablet_service.showWebview(html_file_path)
