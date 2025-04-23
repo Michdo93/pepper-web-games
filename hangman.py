ce40d8e271bc6467a4c210911a3ca7244cada9f3
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2025-04-23 11:01:00 +0200

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -14,7 +14,7 @@ def main(session):
 
     try:
         # Pfad zur HTML-Datei
-        html_file_path = "http://198.18.0.1/apps/voice_handler/hangman.html"
+        html_file_path = "http://198.18.0.1/apps/pepper-web-games/hangman.html"
 
         # Anzeigen der HTML-Seite
         tablet_service.showWebview(html_file_path)
